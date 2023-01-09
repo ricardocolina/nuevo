e789db7c618e6943bfcd0e4a3b063f3b01952fea
ricardocolina <33906430+ricardocolina@users.noreply.github.com>
2023-01-09 11:10:35 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.py
@@ -0,0 +1,11 @@
+def on_forever():
+    pass
+basic.forever(on_forever)
+basic.show_number(2)
+basic.show_number(3)
+led.plot(0, 0)
+min(1, 2)
+1 + 1
+if True:
+    pass
+basic.show_icon(IconNames.HEART)
---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.showNumber(2)
+basic.showNumber(3)
+led.plot(0, 0)
+Math.min(1, 2)
+1 + 1
+if (true) {
+	
+}
+basic.showIcon(IconNames.Heart)
